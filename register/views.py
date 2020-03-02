7073a9f6dafdaf91fe230807eb14c93c6a5a53e6
Som501103 <46430603+Som501103@users.noreply.github.com>
2020-03-02 09:26:03 +0700

    change count

--- register/views.py
+++ register/views.py
@@ -11,6 +11,8 @@ from django.db.models import Q
 
 def home(request):
     courses = Course_D.objects.all().filter(status = 1)
+    count = Course_D['']
+
 
     return render(request, 'home.html', {'courses': courses})
 
@@ -29,6 +31,8 @@ def course_title(request, PK_Course_D):
                 fullname = nameget['TitleFullName']+nameget['FirstName']+' '+nameget['LastName']
                 employee = List_Emp(ref_course=course, E_ID = Emp_id, Fullname= fullname, Position = nameget['PositionDescShort'],Level = nameget['LevelCode'] ,Dep = nameget['DepartmentShort'])
                 employee.save()
+                count = len(List_Emp.objects.get(ref_course=course, status = 1))
+                update_num_student = Course_D.objects.filter(PK_Course_D=course).update(Number_People=count)
                 employee.Fullname
                 massage = "ท่านได้ลงทะเบียนสำเร็จแล้ว"
                 
