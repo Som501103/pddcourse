4800d3e21958e25dbffca52d09248c85ce0200f7
fubukai <karn_muan@hotmail.com>
2021-08-06 09:34:21 +0700

    Update views.py

--- register/views.py
+++ register/views.py
@@ -109,10 +109,10 @@ def home(request):
     print("Current Time =", current_time)'''
     massage = ""
     if Emp_id == '501103' or Emp_id == '503710' or Emp_id == '499781' or Emp_id == '507599' or Emp_id == '492613' or Emp_id == '497784' :
-        openorclose = Course_D.objects.filter(PK_Course_D = 5)
+        openorclose = Course_D.objects.get(PK_Course_D = 5)
         courses = Course_D.objects.all().filter(Access_level = 2).annotate(Gap_number =F('Number_App') - F('Number_People')).order_by('-PK_Course_D') | Course_D.objects.all().filter(PK_Course_D = 3).annotate(Gap_number =F('Number_App') - F('Number_People')).order_by('-PK_Course_D')
     else:  
-        openorclose = Course_D.objects.filter(PK_Course_D = 5)
+        openorclose = Course_D.objects.get(PK_Course_D = 5)
         if Emp_id == '501103' or Emp_id == '503710' or Emp_id == '499781' or Emp_id == '507599' or Emp_id == '492613' or Emp_id == '497784' :
             courses = Course_D.objects.all().annotate(Gap_number =F('Number_App') - F('Number_People')).order_by('-PK_Course_D')
         elif LevelCode == '07' or LevelCode == '08' or LevelCode == 'M1' or LevelCode == 'M2': # เช็คระดับของนักศึกษา ระดับ7-8
